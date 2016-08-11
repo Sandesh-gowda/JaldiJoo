c7d75b16e0a689f215107aa633b2e56a046b9992
Sumeeth <sumeethgowda@gmail.com>
2016-08-11 14:42:07 +0530

    Every thing at once.

---
+++ app/src/main/java/com/creator/jaldijoo/Dashboard.java
@@ -0,0 +1,21 @@
+package com.creator.jaldijoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.aakira.expandablelayout.ExpandableWeightLayout;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dummy_registration_complete1);
+
+
+
+
+
+
+    }
+}
